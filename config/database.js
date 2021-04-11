8dd3c7edfbff9a35237c662f0567e232d302a446
vyskoc <vyskoc@wezeo.com>
2021-04-11 23:30:13 +0200

    dadasd

--- config/database.js
+++ config/database.js
@@ -81,7 +81,7 @@ module.exports = {
       user: Env.get('DB_USER', 'root'),
       password: Env.get('DB_PASSWORD', ''),
       database: Env.get('DB_DATABASE', 'adonis'),
-      //ssl: true
+      ssl: { rejectUnauthorized: false }
     },
     debug: Env.get('DB_DEBUG', false)
   }
