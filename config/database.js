cd8cc03705d88e48a47fc533c213f3601803c100
vyskoc <vyskoc@wezeo.com>
2021-04-11 23:21:22 +0200

    dadasd

--- config/database.js
+++ config/database.js
@@ -50,14 +50,15 @@ module.exports = {
   */
   mysql: {
     client: 'mysql',
-    connection: {
+    connection: Env.get('DATABASE_URL', 'localhost'),
+    /*connection: {
       host: Env.get('DB_HOST', 'localhost'),
       port: Env.get('DB_PORT', ''),
       user: Env.get('DB_USER', 'root'),
       password: Env.get('DB_PASSWORD', ''),
       database: Env.get('DB_DATABASE', 'adonis'),
       ssl: true
-    },
+    },*/
     debug: Env.get('DB_DEBUG', false)
   },
 
