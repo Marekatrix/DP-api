fd7e2cce7cbc5768d58e87ee33d88c160291f7ba
vyskoc <vyskoc@wezeo.com>
2021-04-13 22:29:59 +0200

    afasdaads

--- config/database.js
+++ config/database.js
@@ -74,15 +74,15 @@ module.exports = {
   */
   pg: {
     client: 'pg',
-    //connection: Env.get('DATABASE_URL', 'localhost'),
-    connection: {
+    connection: Env.get('DATABASE_URL', 'localhost'),
+    /*connection: {
       host: Env.get('DB_HOST', 'localhost'),
       port: Env.get('DB_PORT', ''),
       user: Env.get('DB_USER', 'root'),
       password: Env.get('DB_PASSWORD', ''),
       database: Env.get('DB_DATABASE', 'adonis'),
-      ssl: false,//{ rejectUnauthorized: false }
-    },
+      ssl: { rejectUnauthorized: false } // false,
+    },*/
     debug: Env.get('DB_DEBUG', false)
   }
 }
